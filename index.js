b6be0945cb48d9e80686bcac29677ec417c35c1a
hotsuchi <suchitra38das@gmail.com>
2025-05-09 16:30:41 +0530

    connect fontend to backend

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
+
 const express = require("express");
 const app = express();
 const PORT = 5000;
+const cors = require('cors');
+const { User } = require('./dbmodel.js');
 
-app.get("/",(req,res)=>{
-    res.send("express home page")
+app.use(cors());
+app.use(express.json());
+
+app.get('/',async (req,res)=>{
+    const dta = await User.find()
+    res.send(dta);
 })
 
 app.listen(PORT,()=>console.log("running port :",PORT));
--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,8 +1,23 @@
+import { useEffect,useState } from 'react';
+import axios from 'axios';
 
 function App() {
+    const [show,setShow] = useState([]);
+    useEffect(()=>{
+        axios.get('http://localhost:5000/')
+        .then((req)=>{
+            const xy = req.data;
+            setShow(xy)
+        }
+        )
+        .catch((err)=>setShow('faild vai'));
+    },[])
+    
   return (
     <div>
-        React js starting page
+        {show.map((v,i)=> (
+        <li key={i}>{v.uname}:-{v.pass}</li>
+        ))}
     </div>
   );
 }
